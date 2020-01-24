61fc595b5bf33fb108ddf5f1b2ed5ce61c6609a6
mukul kumra <mukul.kumra@ge.com>
2020-01-24 18:23:12 +0530

    changed product model with sequelize

--- Model/Product.js
+++ Model/Product.js
@@ -1,72 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-const helper = require('../helper');
-const pathToProduct = path.join(helper.rootDir, 'data', 'products.json');
-const db = require('../database');
+const Sequelize = require('sequelize');
+const sequelize = require('../database');
 
-module.exports = class Product {
-
-    constructor(title, imageUrl, description, price, id) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-        this.id = id ? id : Math.random();
-
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: falses
     }
+});
 
-    async saveProduct(id, cb) {
-        // helper.fetchProducts(pathToProduct, (products) => {
-        //     if (id) {
-        //         let productToBeEdited = products.find(x => +x.id == +id);
-        //         let prodIndex = products.findIndex(x => +x.id == +id);
-        //         productToBeEdited = { ...this };
-        //         products.splice(prodIndex, 1, productToBeEdited)
 
-        //     } else {
-        //         products.push(this);
-        //     }
-
-        //     fs.writeFile(pathToProduct, JSON.stringify(products), err => {
-        //         if (err)
-        //             console.log(err);
-        //         cb();
-
-        //     });
-        // })
-        if (id) {
-            return db.execute('update product set title=?,description=?,imageUrl=?,price=? where id=?', [this.title, this.description, this.imageUrl, this.price, +id]);
-        }
-        return db.execute('insert into product (title,description,imageUrl,price) va    lues(?,?,?,?)', [this.title, this.description, this.imageUrl, this.price])
-
-    }
-
-    static async fetchAll(cb) {
-        // helper.fetchProducts(pathToProduct, products => {
-        //     cb(products);
-        // })
-        return await db.execute('select * from product');
-    }
-    static async findProductById(id, cb) {
-        // helper.fetchProducts(pathToProduct, products => {
-        //     let product = products.find(p => +p.id === +id);
-        //     product ? cb(product) : cb(null);
-
-        // })
-
-        return db.execute('select * from product where id=?', [+id]);
-    }
-    static deleteProduct(productId, cb) {
-        this.fetchAll((products) => {
-            let prodIndex = products.findIndex(x => +x.id == +productId);
-            products.splice(prodIndex, 1);
-            fs.writeFile(pathToProduct, products, err => {
-                if (err)
-                    console.log(err);
-                cb();
-            })
-
-        })
-    }
-
-}
+module.exports = Product
--- database.js
+++ database.js
@@ -1,10 +1,15 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'NodeApp',
+//     password: 'dunston@123'
+// });
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'NodeApp',
-    password: 'dunston@123'
-});
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('NodeApp', 'root', 'dunston@123', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
 
-module.exports = pool.promise();
+module.exports = sequelize;
